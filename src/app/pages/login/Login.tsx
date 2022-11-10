b6d67766a492ff5921579cc8e3be5f67570c53ca
Matheus Alves <matheusfrancisco0330@gmail.com>
2022-11-10 17:28:53 -0300

    feat: useMemo

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,25 +1,48 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 export const Login = () => {
-const    [email, setEmail] = useState('')
-const    [password, setPassword] = useState('')
-  const entrar = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const emailLength = useMemo(() => {
+    return email.length
+  },[email])
+
+  const handleEntrar = () => {
+    console.log(email);
+    console.log(password);
+    
   };
 
   return (
     <>
+      <p>Qtda email: {emailLength } </p>
       <h1>Login</h1>
-        <form action="">
-            <label htmlFor="email">Email</label>
-            <input type="email" name="" value={email} onChange={e => setEmail(e.target.value)} id="email"/>
+      <form action="">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name=""
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          id="email"
+        />
 
-            <label htmlFor="password">Senha</label>
-            <input type="password" name="" value={password} onChange={e => setPassword(e.target.value)} id="password"/>
-        </form>
-      <button onClick={entrar} type="button">
+        <label htmlFor="password">Senha</label>
+        <input
+          type="password"
+          name=""
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          id="password"
+        />
+      </form>
+      <button onClick={handleEntrar} type="button">
         Entrar
       </button>
-      <p>{email} --- {password}</p>
+      <p>
+        {email} --- {password}
+      </p>
     </>
   );
 };
