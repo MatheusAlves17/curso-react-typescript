8fc2282fcc626df8f748d1f6dff5bd268cacc3a8
Matheus Alves <matheusfrancisco0330@gmail.com>
2022-11-10 20:37:40 -0300

    feat: add useRef with objects

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,15 @@
+import { useRef } from 'react';
 import { Link } from  'react-router-dom';
 export const Dashboard = () => {
+    const counter = useRef({count: 0}) 
     return (
         <>
+            <p>{counter.current.count}</p>
             <h1>Dashboard</h1>
+            <button onClick={() => counter.current.count++}>Somar</button>
+            <button onClick={() => console.log(counter.current.count)}>Log</button>
             <Link to="/entrar">Login</Link>
+            
         </>
     );
 }
